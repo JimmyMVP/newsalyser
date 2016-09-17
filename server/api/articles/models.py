e657a9642e9ac794dd01b8b71ca8dc1a948a88b1
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2016-09-17 12:46:51 +0200

    Added data collection script

--- server/api/articles/models.py
+++ server/api/articles/models.py
@@ -3,23 +3,32 @@ from django.db import models
 
 # Create your models here.
 
+class Sources(models.Model):
+
+    json = models.JSONField()
+    __str__(self):
+        return self.json
 
 
 class Article(models.Model):
 
-    source = models.CharField(max_length = 50, blank = False)
-    
-    original_title = models.CharField(max_length = 50, blank = False)
+    id = models.AutoField(primary_key=True)
 
-    article_text = models.TextField(blank = False)
+    brand = models.CharField(max_length = 50, blank = False)
+    
+    title = models.CharField(max_length = 50, blank = False)
+
     url = models.CharField(max_length = 100, blank = False)
     clustered = models.BooleanField(default = False)
     
     added = models.DateTimeField(auto_now = True)
-    date = models.DateField(blank = False)
 
     cluster = models.ForeignKey("Cluster", null = True)
 
+    json = models.JSONField()
+
+    def __str__(self):
+        return self.json
 
 
 class Cluster(models.Model):
@@ -27,3 +36,5 @@ class Cluster(models.Model):
     cluster_title = models.CharField(max_length = 50, blank = True)
     added = models.DateTimeField(auto_now = True)
 
+    def __str__(self):
+        return cluster_title
--- server/api/collectdata/apis/newspaperAnalysis.py
+++ server/api/collectdata/apis/newspaperAnalysis.py
@@ -1,31 +1,36 @@
 import json
 from watson_developer_cloud import AlchemyLanguageV1
-import requests
-import  pymongo
-from pymongo import MongoClient
 import datetime
 import newspaper
+
+
+from articles.models import Article
 
 def scrape(news_sites=[]):
 
     urlmap = {}
     for site in news_sites:
         news = newspaper.build(site)
+        site = news.brand
         urlmap[site] = []
         for article in news.articles:
-            urlmap[site].append(article.url)
+            urlmap[site].append({"url" : article.url, "top_image" : article.top_image})
+
     return urlmap
 
 
 
-def Extract_Sentiment(api_key="87add3c04a657951ee91eee94d11290b8a734750", sites="http://www.cnn.com/2016/09/18/realestate/so-you-think-your-place-is-small.html?hp&action=click&pgtype=Homepage&clickSource=image&module=photo-spot-region&region=top-news&WT.nav=top-news&mtrref=www.nytimes.com&gwh=689DBA02B8BA5664CCFD84935A2030C3&gwt=pay"):
+def extract_sentiment(api_key="87add3c04a657951ee91eee94d11290b8a734750", sites="http://www.cnn.com/2016/09/18/realestate/so-you-think-your-place-is-small.html?hp&action=click&pgtype=Homepage&clickSource=image&module=photo-spot-region&region=top-news&WT.nav=top-news&mtrref=www.nytimes.com&gwh=689DBA02B8BA5664CCFD84935A2030C3&gwt=pay"):
     alchemy_language = AlchemyLanguageV1(api_key = api_key)
     response = {}
     combined_operations = ['page-image', 'entity', 'keyword', 'title', 'author', 'taxonomy', 'concept', 'doc-emotion']
+
     for curr_site in sites:
         print(curr_site)
 
-        for curr_url in sites[curr_site]:
+        for article in sites[curr_site][:10]:
+
+            curr_url = article.url
             print(curr_url)
             if curr_url=="":
                 continue
@@ -39,14 +44,34 @@ def Extract_Sentiment(api_key="87add3c04a657951ee91eee94d11290b8a734750", sites=
             except:
                 print("excpeption")
                 continue
-            respones_dict = json.loads(curr_response)
-            print(respones_dict)
-            response[respones_dict["url"]] = respones_dict
+            response_dict = json.loads(curr_response)
+            
+            response_dict["brand"] = curr_site
+            response_dict["top_image"] = article["top_image"]
 
-    print(response)
+            Article.create({"title" : response_dict["title"], "url" : response_dict["url"], "brand" : response_dict["brand"], "json" : response_dict, "clustered" : False})
+
+            print(response_dict)
+
+
+
+
+            response[respones_dict["url"]] = respones_dict
+        response_brand
 
 sites  = ["http://www.usatoday.com/","http://www.dailymail.co.uk/home/index.html","https://www.yahoo.com/news/?ref=gs","https://www.theguardian.com/world","http://en.canoe.com/home.html","http://edition.cnn.com/WORLD/","http://www.theage.com.au/","http://www.journalgazette.net/","http://washingtonpost.com/","http://www.nytimes.com/","http://foxnews.com/","http://bbc.com/","http://www.111breakingnews.com/?f"]
-srcs = scrape(sites)
+srcs = scrape(sites[0])
 print(srcs)
 print("*********************************************")
-Extract_Sentiment(sites = srcs)
+
+
+
+
+
+
+
+
+
+
+
+
