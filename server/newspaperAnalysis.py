0059e27839c80487139a8f7b5d245ba3a7c52aca
trrebit Hackathonyan <trrebit Hackathonyan>
2016-09-17 11:43:37 +0200

    Combiend Scraper with Analysis

--- server/newspaperAnalysis.py
+++ server/newspaperAnalysis.py
@@ -4,30 +4,46 @@ import requests
 import  pymongo
 from pymongo import MongoClient
 import datetime
-client = MongoClient()
-client = MongoClient('localhost', 27017)
-db = client.test_database
-db = client['test-database']
-post = {"author": "Mike",
-        "text": "My first blog post!",
-        "tags": ["mongodb", "python", "pymongo"],
-       "date": datetime.datetime.utcnow()}
+import newspaper
 
-def Extract_Sentiment(api_key="87add3c04a657951ee91eee94d11290b8a734750", urls="http://www.cnn.com/2016/09/18/realestate/so-you-think-your-place-is-small.html?hp&action=click&pgtype=Homepage&clickSource=image&module=photo-spot-region&region=top-news&WT.nav=top-news&mtrref=www.nytimes.com&gwh=689DBA02B8BA5664CCFD84935A2030C3&gwt=pay"):
+def scrape(news_sites=[]):
+
+    urlmap = {}
+    for site in news_sites:
+        news = newspaper.build(site)
+        urlmap[site] = []
+        for article in news.articles:
+            urlmap[site].append(article.url)
+    return urlmap
+
+
+
+def Extract_Sentiment(api_key="87add3c04a657951ee91eee94d11290b8a734750", sites="http://www.cnn.com/2016/09/18/realestate/so-you-think-your-place-is-small.html?hp&action=click&pgtype=Homepage&clickSource=image&module=photo-spot-region&region=top-news&WT.nav=top-news&mtrref=www.nytimes.com&gwh=689DBA02B8BA5664CCFD84935A2030C3&gwt=pay"):
     alchemy_language = AlchemyLanguageV1(api_key = api_key)
     response = {}
     combined_operations = ['page-image', 'entity', 'keyword', 'title', 'author', 'taxonomy', 'concept', 'doc-emotion']
-    for curr_url in urls:
-        curr_response = json.dumps(
-          alchemy_language.combined (
-            url=curr_url,
-            extract=combined_operations,
-           ),
-          indent=2)
+    for curr_site in sites:
+        print(curr_site)
+        continue
+        for curr_url in sites[curr_site]:
+            print(curr_url)
+            if curr_url=="":
+                continue
+            curr_response = json.dumps(
+              alchemy_language.combined (
+                url=curr_url,
+                extract=combined_operations,
+               ),
+              indent=2)
 
-        respones_dict = json.loads(curr_response)
+            respones_dict = json.loads(curr_response)
 
         response[respones_dict["url"]] = respones_dict
 
-        print(curr_response)
-Extract_Sentiment(urls =[ "http://www.msn.com/en-us/news/us/two-police-officers-shot-in-philadelphia/ar-BBwgf79?li=BBnb7Kz"])
+    print(response)
+
+sites  = ["http://www.usatoday.com/","http://www.dailymail.co.uk/home/index.html","https://www.yahoo.com/news/?ref=gs","https://www.theguardian.com/world","http://en.canoe.com/home.html","http://edition.cnn.com/WORLD/","http://www.asahi.com/ajw/","http://www.theage.com.au/","http://www.journalgazette.net/","http://washingtonpost.com/","http://www.nytimes.com/","http://foxnews.com/","http://bbc.com/","http://www.111breakingnews.com/?f"]
+srcs = scrape(sites)
+print(srcs)
+print("*********************************************")
+Extract_Sentiment(sites = srcs)
--- server/scraper.py
+++ server/scraper.py
@@ -8,9 +8,12 @@ def scrape(news_sites=[]):
     for site in news_sites:
         news = newspaper.build(site)
         urlmap[site] = []
-        for article in news:
-            urlmap[site] = article.url
+        for article in news.articles:
+            urlmap[site].append(article.url)
+
 
 
     return urlmap
 
+sites  = ["http://www.usatoday.com/","http://www.dailymail.co.uk/home/index.html","https://www.yahoo.com/news/?ref=gs","https://www.theguardian.com/world","http://en.canoe.com/home.html","http://edition.cnn.com/WORLD/","http://www.asahi.com/ajw/","http://www.theage.com.au/","http://www.journalgazette.net/","http://washingtonpost.com/","http://www.nytimes.com/","http://foxnews.com/","http://bbc.com/","http://www.111breakingnews.com/?f"]
+print(scrape(sites))
