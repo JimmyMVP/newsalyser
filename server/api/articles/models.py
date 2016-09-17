fece2d9d0a1d87acd8288e014c26867662437472
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2016-09-17 11:09:06 +0200

    Added first models for database

--- server/api/articles/models.py
+++ server/api/articles/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+
+class Article(models.Model):
+
+    source = models.CharField(max_length = 50, blank = False)
+    
+    original_title = models.CharField(max_length = 50, blank = False)
+
+    article_text = models.TextField(blank = False)
+    url = models.CharField(max_length = 100, blank = False)
+    clustered = models.BooleanField(default = False)
+    
+    added = models.DateTimeField(auto_now = True)
+    date = models.DateField(blank = False)
+
+    cluster = models.ForeignKey("Cluster", null = True)
+
+
+
+class Cluster(models.Model):
+
+    cluster_title = models.CharField(max_length = 50, blank = True)
+    added = models.DateTimeField(auto_now = True)
+
